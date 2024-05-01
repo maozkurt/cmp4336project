f73dd1f097f9778c8da6c44afd65c3e10e374aae
maozkurt <maozkurt12@gmail.com>
2024-05-01 18:08:09 +0300

    change to getdata

--- getdata.py
+++ getdata.py
@@ -1,18 +1,39 @@
 from web3 import Web3
+import json
 
 # get api url from APIURI.txt
 apifile = open("./APIURI.txt")
-APIURI = apifile.readline();
+APIURI = apifile.read().splitlines()[0];
 
 
 w3 = Web3(Web3.HTTPProvider(APIURI))
+print(w3.is_connected())
 
 tokenaddress = Web3.to_checksum_address('0x68749665ff8d2d112fa859aa293f07a622782f38')
 tokenabi = []
+with open("./erc20abi.json", "r" ) as f:
+    tokenabi = json.load(f)
+
 tokencontract = w3.eth.contract(address=tokenaddress, abi=tokenabi)
 
-transactions = tokencontract.events.Transfer().getLogs()
+#event_filter = tokencontract.events.Transfer.create_filter(fromBlock=0x0, toBlock=0xffffffffff)
+#transactions = event_filter.get_new_entries()
 
 with open("./data", "w") as output:
-    output.write(transactions)
-
+    # Get first third of the data, since api limits response size to 10K logs at one request.
+    transactions = tokencontract.events.Transfer().get_logs(fromBlock=0x1169F8B, toBlock=0x11E5BB7)
+    for i in transactions:
+        output.write(str(i))
+        output.write("\n")
+    
+    # Second third
+    transactions = tokencontract.events.Transfer().get_logs(fromBlock=0x11E5BB8, toBlock=0x1260BC7)
+    for i in transactions:
+        output.write(str(i))
+        output.write("\n")
+    
+    # Last third
+    transactions = tokencontract.events.Transfer().get_logs(fromBlock=0x1260BC8, toBlock=0x12DC1E6)
+    for i in transactions:
+        output.write(str(i))
+        output.write("\n")
